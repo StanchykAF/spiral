6832fdadf0963704c46e0374f7b28394d19865ae
Nastassia Stanchyk <stanchikaf@gmail.com>
2022-04-08 20:43:56 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -2,6 +2,47 @@ package com.epam.rd.autotasks;
 
 class Spiral {
     static int[][] spiral(int rows, int columns) {
-        return null;
+        int[][] matrix = new int[rows][columns];
+        int number = 1, i = 0, j = 0;
+        int flagToRight = columns, flagToLeft= 0, flagToDown = rows, flagToUp = 0;
+
+        while (flagToRight > flagToLeft && flagToDown > flagToUp) {
+            //if (flagToRight < flagToLeft || flagToDown < flagToUp) break;
+            for (; j < flagToRight; j++) {
+                matrix[i][j] = number++;
+            }
+
+            i++;
+            flagToUp++;
+            j--;
+
+            if (flagToDown <= flagToUp) break;
+            for (; i < flagToDown; i++) {
+                matrix[i][j] = number++;
+            }
+
+            flagToRight--;
+            i--;
+            j--;
+
+            if (flagToRight <= flagToLeft) break;
+            for (; j>=flagToLeft; j--) {
+                matrix[i][j] = number++;
+            }
+
+            flagToDown--;
+            i--;
+            j++;
+
+            if (flagToDown <= flagToUp) break;
+            for (; i >= flagToUp; i--) {
+                matrix[i][j] = number++;
+            }
+
+            i++;
+            j++;
+            flagToLeft++;
+        }
+        return matrix;
     }
 }
